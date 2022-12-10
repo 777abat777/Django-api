0e159c30c9ef426ad0ca452a4848466620ee00d5
777abat777 <87979902+777abat777@users.noreply.github.com>
2022-12-10 14:17:53 +0300

    test comit

--- myapi/models.py
+++ myapi/models.py
@@ -7,3 +7,11 @@ class Hero(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class User(models.Model):
+    name = models.CharField(max_length=60)
+    status = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
